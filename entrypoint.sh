ff947c32dcbf6c8a234cc338135e0cfe692deca8
Darcy DeClute <96313181+scrumtuous@users.noreply.github.com>
2023-04-20 14:09:34 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,47 +10,85 @@ if [[ "$INPUT_ADOC_FILE_EXT" != .* ]]; then
 fi
 
 echo "Configure git"
-apk add openssh-client -q > /dev/null
-
 git config --global --add safe.directory /github/workspace
 git fetch --all
 
-# Checkout to destination working branch
-echo "Checking out the ${GITHUB_REF##*/} branch (keeping its history) from commit $GITHUB_SHA"
-git checkout "$GITHUB_SHA" -B ${GITHUB_REF##*/}
+# Checks if the user has provided a specific source dir different from the root.
+# Only the files in the source dir are processed.
+HAS_SOURCE_DIR=true
+if [[ "$INPUT_SOURCE_DIR" == "." || "$INPUT_SOURCE_DIR" == "./" ]]; then
+  HAS_SOURCE_DIR=false
+fi
+
+# If a source dir was provided, remove all other files and directories and
+# keept just the ones in the source dir, moving them to the root dir.
+if [[ $HAS_SOURCE_DIR == true ]]; then
+  echo "Checking out the gh-pages branch on $INPUT_SOURCE_DIR (without keeping its history) from commit $GITHUB_SHA"
+  git branch -D gh-pages 1>/dev/null 2>/dev/null || true
+  git checkout -q --orphan gh-pages "$GITHUB_SHA" 1>/dev/null
+  mv "$INPUT_SOURCE_DIR" /tmp/source
+  #Ignores directories . and .git
+  find . -not -path './.git*' -not -name '.' -exec rm -rf {} \; || true
+  mv /tmp/source/* .
+  git add .
+else
+  echo "Checking out the gh-pages branch (keeping its history) from commit $GITHUB_SHA"
+  git checkout "$GITHUB_SHA" -B gh-pages
+fi
 
 # Executes any arbitrary shell command (such as packages installation and environment setup)
 # before starting build.
 # If no command is provided, the default value is just an echo command.
 eval "$INPUT_PRE_BUILD"
 
-echo "Converting AsciiDoc files to HTML and PDF"
-eval find . $INPUT_FIND_PARAMS -name "*$INPUT_ADOC_FILE_EXT" -exec asciidoctor -b html $INPUT_ASCIIDOCTOR_PARAMS {} \\\;
-eval find . $INPUT_FIND_PARAMS -name "*$INPUT_ADOC_FILE_EXT" -exec asciidoctor-pdf -b pdf -a pdf-stylesdir=themes -a pdf-style=basic -a pdf-fontsdir=./fonts $INPUT_ASCIIDOCTOR_PARAMS {} \\\;
-find . -name "README.html" -execdir ln -s "README.html" "index.html" \;
+if [[ $INPUT_SLIDES_SKIP_ASCIIDOCTOR_BUILD == false ]]; then
+    echo "Converting AsciiDoc files to HTML"
+    find . -name "*$INPUT_ADOC_FILE_EXT" -exec asciidoctor -b html $INPUT_ASCIIDOCTOR_PARAMS {} \;
+    find . -name "README.html" -execdir ln -s "README.html" "index.html" \;
+    find . -name "*$INPUT_ADOC_FILE_EXT" -exec git rm -f --cached {} \;
+fi
 
+PDF_FILE="ebook.pdf"
+if [[ $INPUT_PDF_BUILD == true ]]; then
+    INPUT_EBOOK_MAIN_ADOC_FILE="$INPUT_EBOOK_MAIN_ADOC_FILE$INPUT_ADOC_FILE_EXT"
+    MSG="Building $PDF_FILE ebook from $INPUT_EBOOK_MAIN_ADOC_FILE"
+    echo "$MSG"
+    asciidoctor-pdf "$INPUT_EBOOK_MAIN_ADOC_FILE" -o "$PDF_FILE" $INPUT_ASCIIDOCTOR_PARAMS
+fi
 
-ls
-
-ls -a
-
-echo "HELLO!"
-
+SLIDES_FILE="slides.html"
+if [[ $INPUT_SLIDES_BUILD == true ]]; then
+    echo "Build AsciiDoc Reveal.js slides"
+    INPUT_SLIDES_MAIN_ADOC_FILE="$INPUT_SLIDES_MAIN_ADOC_FILE$INPUT_ADOC_FILE_EXT"
+    MSG="Building $SLIDES_FILE with AsciiDoc Reveal.js from $INPUT_SLIDES_MAIN_ADOC_FILE"
+    echo "$MSG"
+    asciidoctor-revealjs -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 "$INPUT_SLIDES_MAIN_ADOC_FILE" -o "$SLIDES_FILE"
+fi
 
 # Executes any post-processing command provided by the user, before changes are committed.
 # If no command is provided, the default value is just an echo command.
 echo "Running post build command."
 eval "$INPUT_POST_BUILD"
 
-echo "Adding output files to ${GITHUB_REF##*/} branch."
-find . -name "*.pdf" -exec git add -f {} \;
-find . -name "*.html" -exec git add -f {} \;
+echo "Adding output files to gh-pages branch."
+if [[ $INPUT_SLIDES_SKIP_ASCIIDOCTOR_BUILD == false ]]; then
+    find . -name "*.html" -exec git add -f {} \;
+fi
+
+if [[ $INPUT_PDF_BUILD == true ]]; then
+    git add -f "$PDF_FILE";
+fi
+
+if [[ $INPUT_SLIDES_BUILD == true ]]; then
+    git add -f "$SLIDES_FILE";
+fi
 
 # Changes in gh-pages branch will be shown as the "GitHub Action" user.
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
-MSG="Convert $INPUT_ADOC_FILE_EXT Files into PDF/HTML from $GITHUB_SHA"
+MSG="Build $INPUT_ADOC_FILE_EXT Files for GitHub Pages from $GITHUB_SHA"
+git rm -rf .github/ || true
 echo "Committing changes to gh-pages branch"
 git commit -m "$MSG" 1>/dev/null
 
@@ -69,4 +107,4 @@ if ! ssh -T git@github.com > /dev/null 2>/dev/null; then
 fi
 
 echo "Pushing changes back to the remote repository"
-git push -f --set-upstream origin ${GITHUB_REF##*/}
+git push -f --set-upstream origin gh-pages
