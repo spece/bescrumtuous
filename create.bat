e51e879deffc0bec44e3541b3b0c31b9e5bf36e9
Cameron <me@example.com>
2023-05-03 19:34:08 -0400

    stuff

--- create.bat
+++ create.bat
@@ -1,4 +1,4 @@
-asciidoctor-pdf -a pdf-theme=some-theme.yml -a pdf-fontsdir=./fonts 00A-scrum-book.txt -o scrum12.pdf
+asciidoctor-pdf -a pdf-theme=theme.yml -a media=prepress -a pdf-fontsdir=./fonts *.adoc
 
 
 # asciidoctor-pdf -a pdf-theme=some-theme.yml -a pdf-fontsdir=tutorial/fonts scrum-book.txt -o scrum100.pdf
